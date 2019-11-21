20233f71bb6f4f9ffab1f1cba978eb0f8996e68f
AdhyyanNarang <adhyyan-n@berkeley.edu>
2019-11-20 23:51:25 -0800

    Interval bound code is now running and fixed small import issues

--- utils/ridge_poisoning.py
+++ utils/ridge_poisoning.py
@@ -4,6 +4,7 @@ import cvxpy as cp
 l = lambda X: np.linalg.norm(X@np.linalg.pinv(X)@y-y)**2
 
 def solve_nominal(X, y, lamb=10):
+    n, m = X.shape
     w = cp.Variable((m,1))
     nominal_obj = cp.Minimize(cp.square(cp.norm(X@w - y, 2)) + lamb*cp.square(cp.norm(w,2)))
     constraints = []
@@ -42,6 +43,32 @@ def solve_poisoned_relaxed(X_nominal, y, lamb=10, rho=10, mu=10):
         N[m:,0:m] == X,
         N[0:m,m:] == X.T,
         
+    ]
+    for i in range(n):
+        constraints.append(cp.norm(X[i]-X_nominal[i],2) <= rho)
+    prob = cp.Problem(poisoned_obj, constraints)
+    prob.solve()
+    return -1*prob.value, X.value, U.value
+
+def solve_poisoned_relaxed_interval_bounded(X_nominal, y, U_lower, U_upper, lamb=10, rho=10, mu=10):
+    n, m = X_nominal.shape
+    X = cp.Variable((n,m))
+    M = cp.Variable((m+1,m+1), PSD=True)
+    N = cp.Variable((m+n,m+n), PSD=True)
+    t = cp.Variable(1)
+    U = cp.Variable((m,m), PSD=True)
+    poisoned_obj = cp.Minimize(t - cp.square(cp.norm(y)))
+    constraints = [
+        M[0:m,0:m] == U - lamb*np.eye(m),
+        M[m,m] == t,
+        M[m:m+1,0:m] == y.T@X,
+        M[0:m,m:m+1] == X.T@y,
+        N[0:m,0:m] == U,
+        N[m:,m:] == np.eye(n),
+        N[m:,0:m] == X,
+        N[0:m,m:] == X.T,
+        U <= U_upper, 
+        U >= U_lower
     ]
     for i in range(n):
         constraints.append(cp.norm(X[i]-X_nominal[i],2) <= rho)
